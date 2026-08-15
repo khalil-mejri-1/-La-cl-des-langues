2c435eed6c6c59141bd91043d983f6d2fa106bfa
khalil-mejri-1 <mejrik1888@gmail.com>
2026-08-15 13:30:19 +0100

    final update

--- client/src/config.js
+++ client/src/config.js
@@ -4,5 +4,5 @@
 // ============================================
 
 
-// export const API_BASE_URL = 'https://la-cl-des-langues-999i.vercel.app';
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://la-cl-des-langues-999i.vercel.app';
+// export const API_BASE_URL = 'http://localhost:5000';
